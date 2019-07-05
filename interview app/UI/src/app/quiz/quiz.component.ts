2858ce1ecbbb27bac4e0cbd527bcdabed766c17c
yashi.mehta <yashi.mehta@YI1008815LT.Yash.com>
2019-07-05 18:42:07 +0530

    Adding few functionalities to quiz

--- interview app/UI/src/app/quiz/quiz.component.ts
+++ interview app/UI/src/app/quiz/quiz.component.ts
@@ -11,7 +11,7 @@ export class QuizComponent implements OnInit {
 
   quizQues: QuizModel[] = []
   ques: QuizModel[] = [];
-  correctAnswers: number = 0;
+  correctAnswers: QuizModel[] = [];
   showResults: boolean = false;
 
   constructor(private route: ActivatedRoute,
@@ -22,20 +22,24 @@ export class QuizComponent implements OnInit {
   ngOnInit() { }
 
   mcqAnswer(questionId, answer) {
-
+    var quesFound: QuizModel= {}
     console.log("question" + questionId + "answer" + answer)
     this.ques = this.quizQues.filter(x => x.qid === questionId);
-    if (this.ques[0].answer == answer) {
-      this.correctAnswers += 1;
+    quesFound = this.correctAnswers.find(x => x.qid === questionId);
+    if (this.ques[0].answer == answer && quesFound == undefined) {
+      this.correctAnswers.push(this.ques[0]);
     }
-    
+    else if(quesFound != undefined && this.ques[0].answer != answer){
+      this.correctAnswers.splice(this.correctAnswers.indexOf(quesFound), 1);
+    }
+
   }
 
-  endQuiz(){
-    console.log("total quest"+this.quizQues.length )
-    console.log("correct answers"+ this.correctAnswers)
+  endQuiz() {
+    console.log("total quest" + this.quizQues.length)
+    console.log("correct answers" + this.correctAnswers.length)
     this.showResults = true;
-    
+
   }
 
 }
